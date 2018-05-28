c11c3e6a2bc2b4b3cf750fafbd01cab7b5a1b94f
Stefen Hasselberg <stefen74@gmail.com>
2018-05-28 22:32:39 +1200

    End of section 9

---
+++ src/app/common/form/dateInput.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Form, Label } from "semantic-ui-react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import moment from "moment";
+import { change } from "redux-form";
+
+const DateInput = ({
+  input: { value, onChange, ...restInput },
+  width,
+  placeholder,
+  meta: { touched, error },
+  ...rest
+}) => {
+  return (
+    <Form.Field error={touched && !!error} width={width}>
+      <DatePicker
+        {...rest}
+        placeholder={placeholder}
+        selected={value ? moment(value) : null}
+        onChange={onChange}
+        {...restInput}
+      />
+      {touched &&
+        error && (
+          <Label basic color="red">
+            {error}
+          </Label>
+        )}
+    </Form.Field>
+  );
+};
+
+export default DateInput;
--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,5 +1,5 @@
 import { combineReducers } from "redux";
-
+import { reducer as formReducer } from "redux-form";
 import testReducer from "../../features/testarea/testReducer";
 import eventReducer from "../../features/event/eventReducer";
 
@@ -7,7 +7,8 @@ console.log("test reducer ", testReducer);
 
 const rootReducer = combineReducers({
   test: testReducer,
-  events: eventReducer
+  events: eventReducer,
+  form: formReducer
 });
 
 export default rootReducer;
