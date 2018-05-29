584bea48abe7e76fb26a66e23c6135cea211740f
Stefen Hasselberg <stefen74@gmail.com>
2018-05-29 22:31:38 +1200

    End of Section 10

--- src/features/testarea/TestArea.jsx
+++ src/features/testarea/TestArea.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Button } from 'semantic-ui-react'
-import { incrementCounter, decrementCounter } from './testActions'
-import Script from 'react-load-script'
-import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete'
+import { Button, Icon } from "semantic-ui-react";
+import { incrementCounter, decrementCounter } from "./testActions";
+import Script from "react-load-script";
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng
+} from "react-places-autocomplete";
+import GoogleMapReact from "google-map-react";
 
 const mapState = state => {
   return {
@@ -14,52 +18,73 @@ const mapState = state => {
 const actions = {
   incrementCounter,
   decrementCounter
-}
+};
+
+const Marker = () => <Icon name="marker" size="big" color="red" />;
 
 class TestArea extends Component {
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
+  };
+
   state = {
-    address: '',
+    address: "",
     scriptLoaded: false
   };
 
-  handleFormSubmit = (event) => {
-    event.preventDefault()
+  handleFormSubmit = event => {
+    event.preventDefault();
 
     geocodeByAddress(this.state.address)
       .then(results => getLatLng(results[0]))
-      .then(latLng => console.log('Success', latLng))
-      .catch(error => console.error('Error', error))
-  }
+      .then(latLng => console.log("Success", latLng))
+      .catch(error => console.error("Error", error));
+  };
 
-  handleScriptLoad = () => this.setState({ scriptLoaded: true })
+  handleScriptLoad = () => this.setState({ scriptLoaded: true });
 
-  onChange = (address) => this.setState({ address })
+  onChange = address => this.setState({ address });
 
   render() {
-    const { incrementCounter, decrementCounter, data } = this.props
-    const { scriptLoaded } = this.state
+    const { incrementCounter, decrementCounter, data } = this.props;
+    const { scriptLoaded } = this.state;
     const inputProps = {
       value: this.state.address,
-      onChange: this.onChange,
-    }
+      onChange: this.onChange
+    };
     return (
       <div>
-        <Script
+        {/* <Script
           url="https://maps.googleapis.com/maps/api/js?key=AIzaSyA4RoKZ1sez35FXDVIy2qWg0tHtR40LoGY&libraries=places"
           onLoad={this.handleScriptLoad}
-        />
+        /> */}
         <h1>Test Area</h1>
         <h3>the answer is: {data}</h3>
         <Button color="green" content="increment" onClick={incrementCounter} />
         <Button color="red" content="decrement" onClick={decrementCounter} />
         <br />
         <br />
-        {scriptLoaded &&
+        {scriptLoaded && (
           <form onSubmit={this.handleFormSubmit}>
             <PlacesAutocomplete inputProps={inputProps} />
             <button type="submit">Submit</button>
           </form>
-        }
+        )}
+        <div style={{ height: "300px", width: "100%" }}>
+          <GoogleMapReact
+            bootstrapURLKeys={{
+              key: "AIzaSyA4RoKZ1sez35FXDVIy2qWg0tHtR40LoGY"
+            }}
+            defaultCenter={this.props.center}
+            defaultZoom={this.props.zoom}
+          >
+            <Marker lat={59.955413} lng={30.337844} text={"Kreyser Avrora"} />
+          </GoogleMapReact>
+        </div>
       </div>
     );
   }
